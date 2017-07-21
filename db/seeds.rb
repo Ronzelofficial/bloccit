b1bff6b6fbdde255e84b528e22b5e4d466ec6754
Ronen Reouveni <ronenreouveni@gmail.com>
2017-07-21 11:32:54 -0700

    assignment21

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,16 @@ topics = Topic.all
     body:   RandomData.random_paragraph
   )
 end
+
+50.times do
+	SponsoredPost.create!(
+		topic: topics.sample,
+		title: RandomData.random_sentence,
+		body: RandomData.random_paragraph,
+		price: 1
+	)
+end
+
 posts = Post.all
 
 # Create Comments
@@ -34,4 +44,5 @@ end
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
