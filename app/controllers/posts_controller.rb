b78ae3e5d0c857e6aafed64f5b6636d7995d948f
Ronen Reouveni <ronenreouveni@gmail.com>
2017-07-28 13:29:36 -0700

    checkpoint26

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,8 @@ class PostsController < ApplicationController
   #   # end
   # end
 
+  before_action :require_sign_in, except: :show
+
   def show
     @post = Post.find(params[:id])
   end
@@ -19,13 +21,9 @@ class PostsController < ApplicationController
 
     def create
     # #9
-      @post = Post.new
-      @post.title = params[:post][:title]
-      @post.body = params[:post][:body]
-
       @topic = Topic.find(params[:topic_id])
-# #35
-      @post.topic = @topic
+      @post = @topic.posts.build(post_params)
+      @post.user = current_user
 
     # #10
       if @post.save
@@ -58,8 +56,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    @post.assign_attributes(post_params)
 
     if @post.save
       flash[:notice] = "Post was updated."
@@ -69,4 +66,9 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+  
+  private
+def post_params
+  params.require(:post).permit(:title, :body)
+end
 end
